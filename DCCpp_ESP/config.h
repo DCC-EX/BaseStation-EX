f4772a39b5451d7b3b8f3aa5bbe64a08bc88879f
Mike Dunston <m_dunston@comcast.net>
2017-07-27 14:19:32 -0700

    new web ui with multi-engine throttle, there is a bug with e-stop button causing the socket to close to be worked out still

--- DCCpp_ESP/config.h
+++ DCCpp_ESP/config.h
@@ -8,11 +8,9 @@ Part of DCC++ BASE STATION for the Arduino / ESP8266
 
 #define DCCPP_CLIENT_PORT 2560
 #define MAX_DCCPP_CLIENTS 10
+#define MAX_WEBSOCKET_CLIENTS 10
 
 #define SERIAL_LINK_SPEED 115200L
 #define SERIAL_LINK_DEV Serial
 
-#define OTA_PORT 8266
-#define OTA_ENABLED true
-#define OTA_HOSTNAME ""
-#define OTA_PASSWORD ""
+#define HOSTNAME "DCCppESP"
